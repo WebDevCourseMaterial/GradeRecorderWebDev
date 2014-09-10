c987a5c4a4a222e5a570d3c6d5065b93845f6f95
David Fisher <fisherds@rose-hulman.edu>
2014-09-10 00:46:35 -0400

    Stripping out solution code

--- handlers/base_handlers.py
+++ handlers/base_handlers.py
@@ -14,16 +14,5 @@ class BasePage(webapp2.RequestHandler):
 
 
 class BaseAction(webapp2.RequestHandler):
-  def post(self):
-    user = users.get_current_user()
-    if not user:
-      self.redirect(users.create_login_url(self.request.uri))
-      return
-    next_active_assignemnt = self.post_for_user(user)
-    if next_active_assignemnt:
-      self.redirect("/?active_assignemnt=" + next_active_assignemnt)
-    else:
-      self.redirect("/")
-
-  def post_for_user(self, user):
-    raise Exception("Subclasses must override this method")
+  pass
+  # TODO: Implement
--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ def get_parent_key(user):
 
 def get_assignments(user):
     """ Gets all of the assignments for this user and makes a key map for them. """
-    assignments = Assignment.query(ancestor=get_parent_key(user)).order(Assignment.name).fetch()
+    assignments = # TODO: Query for all Assignments for this user order by name, then fetch()
     assignments_map = {}
     for assignment in assignments:
         assignments_map[assignment.key] = assignment
@@ -16,7 +16,7 @@ def get_assignments(user):
 
 def get_students(user):
     """ Gets all of the students for this user and makes a key map for them. """
-    students = Student.query(ancestor=get_parent_key(user)).order(Student.rose_username).fetch()
+    students = # TODO: Query for all Students for this user order by rose_username, then fetch()
     students_map = {}
     teams = []
     for student in students:
@@ -29,7 +29,7 @@ def get_students(user):
 def get_grade_entries(user, assignments_map, students_map):
     """ Gets all of the grade entries for this user.
           Replaces the assignment_key and student_key with an assignment and student. """
-    grade_entries = GradeEntry.query(ancestor=get_parent_key(user)).fetch()
+    grade_entries = # TODO: Query for all GradeEntries for this user , then fetch()
     for grade_entry in grade_entries:
         grade_entry.assignment = assignments_map[grade_entry.assignment_key]
         grade_entry.student = students_map[grade_entry.student_key]
@@ -38,23 +38,23 @@ def get_grade_entries(user, assignments_map, students_map):
 
 def remove_all_grades_for_assignment(user, assignment_key):
   """ Removes all grades for the given assignment. """
-  grades_for_assignment_query = GradeEntry.query(ancestor=assignment_key)
+  grades_for_assignment_query = # TODO: Query for all GradeEntries for assignment_key
   for grade in grades_for_assignment_query:
     grade.key.delete()
 
 
 def remove_all_grades_for_student(user, student_key):
   """ Removes all grades for the given student. """
-  grades_for_student_query = GradeEntry.query(GradeEntry.student_key==student_key, ancestor=get_parent_key(user))
+  grades_for_student_query = # TODO: Query for all GradeEntries for this user and student_key
   for grade in grades_for_student_query:
     grade.key.delete()
 
 
 def remove_all_students(user):
   """ Removes all grades and all students for a user. (use with caution) """
-  all_grades_query = GradeEntry.query(ancestor=get_parent_key(user))
+  all_grades_query = # TODO: Query for all GradeEntries for this user
   for grade in all_grades_query:
     grade.key.delete()
-  all_students_query = Student.query(ancestor=get_parent_key(user))
+  all_students_query = # TODO: Query for all Students for this user
   for student in all_students_query:
     student.key.delete()
