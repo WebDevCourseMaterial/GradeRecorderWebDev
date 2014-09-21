f6a914453336642e0bec33ee6def95388912f179
David Fisher <fisherds@rose-hulman.edu>
2014-09-21 00:58:22 -0400

    minor refactor on InsertAssignment

--- handlers/insert_handlers.py
+++ handlers/insert_handlers.py
@@ -23,15 +23,14 @@ class InsertAssignmentAction(webapp2.RequestHandler):
   def post(self):
     user = users.get_current_user()
     urlsafe_entity_key = self.request.get('assignment_entity_key')
-
     if len(urlsafe_entity_key) > 0:
       # Edit
       assignment_key = ndb.Key(urlsafe=urlsafe_entity_key)
       assignment = assignment_key.get()
-      assignment.name = self.request.get('assignment_name')
     else:
-      assignment = Assignment(parent=utils.get_parent_key(user),
-                              name=self.request.get('assignment_name'))
+      # Add
+      assignment = Assignment(parent=utils.get_parent_key(user))
+    assignment.name = self.request.get('assignment_name')
     assignment.put()
     self.redirect("/?active_assignment=" + assignment.key.urlsafe())
 
